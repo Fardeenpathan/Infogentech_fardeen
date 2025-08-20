3e9afb6de5b58532c5096a191ede392897b16eb9
Sonalika <sonalika.infogentech@gmail.com>
2025-08-20 18:16:46 +0530

    header right part done

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,12 +1,13 @@
 import Link from 'next/link'
 import { Button } from '@/components/ui/button'
 import Image from 'next/image'
+import '../app/globals.css'
 
 export function Navbar() {
   return (
     <nav className="flex bg-transparent top-6.25 fixed w-full justify-between items-center z-50 ">
       {/* Logo and brand */}
-      <div className="flex items-center ml-38 border-1 border-[#8E8E8E] rounded-md ">
+      <div className="flex items-center border-1 border-[#8E8E8E] rounded-md ml-38">
         <div className="flex items-center pl-3.75 py-2.25 space-x-1.5">
           {/* Logo icon - exact purple grid */}
           {/* <div className="w-8 h-8 bg-gradient-to-br from-purple-500 to-purple-600 rounded-md flex items-center justify-center p-1">
@@ -58,24 +59,51 @@ export function Navbar() {
       {/* </div> */}
 
       {/* Right side navigation */}
-      <div className="flex items-center space-x-6">
-        <div className="hidden md:flex items-center space-x-6">
-          <Link href="#" className="text-white/90 hover:text-white transition-colors text-sm">
+      <div className="flex items-center border-1 border-[#8E8E8E] rounded-md mr-38 pr-3.75">
+        <div className="hidden md:flex items-center space-x-12.5 px-12.5 py-4.5 text-lg font-medium font-Jost">
+          <Link href="#">
             Blogs
           </Link>
-          <Link href="#" className="text-white/90 hover:text-white transition-colors text-sm">
+          <Link href="#" >
             Contact us
           </Link>
-          <Link href="#" className="text-white/90 hover:text-white transition-colors text-sm">
+          <Link href="#" >
             About us
           </Link>
         </div>
-        <Button 
+
+
+          {/* <div className="px-6.75 py-3.25 gradient-border rounded-md ">
+  <Link href="/" className="text-white">Free Consultation</Link>
+        </div> */}
+
+
+<div className="p-[4px] rounded-[10px] bg-gradient-to-r from-[#7544E4] to-[#FFFFFF] inline-block">
+  <div className="rounded-md bg-black px-6 py-3">
+    <Link href="/" className="text-white">Free Consultation</Link>
+  </div>
+</div>
+
+
+
+       
+
+  {/* <div className="px-6.75 py-3.25 bg-gradient-to-r from-[#7544E4] to-[#FFFFFF] rounded-md">
+
+   <div className="bg-transparent rounded-md px-6.75 py-3.25">
+    <Link href="/">Free Consultation</Link>
+  </div> */}
+  {/* <Link href="/">Free Consultation</Link>
+{/* </div> */} 
+
+
+
+        {/* <Button 
           variant="outline" 
           className="bg-transparent border-purple-500 text-white hover:bg-purple-600 hover:border-purple-600 text-sm px-4 py-2"
-        >
-          Free Consultation
-        </Button>
+        > */}
+          {/* Free Consultation
+        </Button> */}
       </div>
     </nav>
   )
