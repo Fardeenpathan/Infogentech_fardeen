db364d79ecfcd5d0cff208e3b561881bbe6540aa
Sonalika <sonalika.infogentech@gmail.com>
2025-08-22 16:40:10 +0530

    subcategories done to some extent

--- src/components/services-dropdown.tsx
+++ src/components/services-dropdown.tsx
@@ -3,6 +3,7 @@
 // src/components/ui/dropdown.tsx
 import Link from "next/link";
 import { useState } from "react";
+import Image  from "next/image";
 
 interface ItemsSubcategory {
   href: string;
@@ -25,25 +26,25 @@ const servicesMenu: ServicesMenuItem[] = [
       {
         href: "/services/graphic-design",
         label: "Graphic Design",
-        imgUrl: "/images/graphic-design.svg",
+        imgUrl: "/ServicesSubcategory.svg",
         description: "Designing impactful graphics for various media.",
       },
       {
         href: "/services/ui-ux-design",
         label: "UI/UX Design",
-        imgUrl: "/images/ux-ui-design.svg",
+        imgUrl: "/ServicesSubcategory.svg",
         description: "Creating intuitive and engaging user experiences.",
       },
       {
         href: "/services/branding",
         label: "Branding",
-        imgUrl: "/images/ux-ui-design.svg",
+        imgUrl: "/ServicesSubcategory.svg",
         description: "Creating intuitive and engaging user experiences.",
       },
       {
         href: "/services/logo-design",
         label: "Logo Design",
-        imgUrl: "/images/ux-ui-design.svg",
+        imgUrl: "/ServicesSubcategory.svg",
         description: "Creating intuitive and engaging user experiences.",
       },
     ],
@@ -55,25 +56,25 @@ const servicesMenu: ServicesMenuItem[] = [
       {
         href: "/services/web-development",
         label: "Web & App Development",
-        imgUrl: "/images/web-development.svg",
+        imgUrl: "/ServicesSubcategory.svg",
         description: "Developing responsive and high-performance websites.",
       },
       {
         href: "/services/mobile-app-development",
         label: "CMS Development",
-        imgUrl: "/images/mobile-app-development.svg",
+        imgUrl: "/ServicesSubcategory.svg",
         description: "Creating mobile applications for iOS and Android.",
       },
       {
         href: "/services/mobile-app-development",
         label: "Custom Integrations",
-        imgUrl: "/images/mobile-app-development.svg",
+        imgUrl: "/ServicesSubcategory.svg",
         description: "Creating mobile applications for iOS and Android.",
       },
       {
         href: "/services/mobile-app-development",
         label: "AI & Cloud Solutions",
-        imgUrl: "/images/mobile-app-development.svg",
+        imgUrl: "/ServicesSubcategory.svg",
         description: "Creating mobile applications for iOS and Android.",
       },
     ],
@@ -85,25 +86,25 @@ const servicesMenu: ServicesMenuItem[] = [
       {
         href: "/services/web-development",
         label: "Photo & Video Production",
-        imgUrl: "/images/web-development.svg",
+        imgUrl: "/ServicesSubcategory.svg",
         description: "Developing responsive and high-performance websites.",
       },
       {
         href: "/services/mobile-app-development",
         label: "Visual Content Design",
-        imgUrl: "/images/mobile-app-development.svg",
+        imgUrl: "/ServicesSubcategory.svg",
         description: "Creating mobile applications for iOS and Android.",
       },
       {
         href: "/services/mobile-app-development",
         label: "Auto & Motion Graphics",
-        imgUrl: "/images/mobile-app-development.svg",
+        imgUrl: "/ServicesSubcategory.svg",
         description: "Creating mobile applications for iOS and Android.",
       },
       {
         href: "/services/mobile-app-development",
         label: "Blog Posts & Articles",
-        imgUrl: "/images/mobile-app-development.svg",
+        imgUrl: "/ServicesSubcategory.svg",
         description: "Creating mobile applications for iOS and Android.",
       },
     ],
@@ -115,44 +116,44 @@ const servicesMenu: ServicesMenuItem[] = [
       {
         href: "/services/web-development",
         label: "Marketing Strategy",
-        imgUrl: "/images/web-development.svg",
+        imgUrl: "/ServicesSubcategory.svg",
         description: "Developing responsive and high-performance websites.",
       },
       {
         href: "/services/mobile-app-development",
         label: "Performance Marketing",
-        imgUrl: "/images/mobile-app-development.svg",
+        imgUrl: "/ServicesSubcategory.svg",
         description: "Creating mobile applications for iOS and Android.",
       },
       {
         href: "/services/mobile-app-development",
         label: "CRM & Automation",
-        imgUrl: "/images/mobile-app-development.svg",
+        imgUrl: "/ServicesSubcategory.svg",
         description: "Creating mobile applications for iOS and Android.",
       },
       {
         href: "/services/mobile-app-development",
         label: "Audience Engagement Strategy",
-        imgUrl: "/images/mobile-app-development.svg",
+        imgUrl: "/ServicesSubcategory.svg",
         description: "Creating mobile applications for iOS and Android.",
       },
 
       {
         href: "/services/mobile-app-development",
         label: "Search Engine Optimization (SEO)",
-        imgUrl: "/images/mobile-app-development.svg",
+        imgUrl: "/ServicesSubcategory.svg",
         description: "Creating mobile applications for iOS and Android.",
       },
       {
         href: "/services/mobile-app-development",
         label: "Content Marketing",
-        imgUrl: "/images/mobile-app-development.svg",
+        imgUrl: "/ServicesSubcategory.svg",
         description: "Creating mobile applications for iOS and Android.",
       },
       {
         href: "/services/mobile-app-development",
         label: "Analysis & Reporting",
-        imgUrl: "/images/mobile-app-development.svg",
+        imgUrl: "/ServicesSubcategory.svg",
         description: "Creating mobile applications for iOS and Android.",
       },
     ],
@@ -169,7 +170,7 @@ export function ServicesDropdown() {
   return (
     <div className="inline-flex relative group">
       <button className="cursor-pointer focus:outline-none">Services</button>
-      <div className=" absolute group-hover:flex hidden  bg-white shadow-lg rounded-md  ">
+      <div className=" absolute group-hover:flex hidden  shadow-lg rounded-md  ">
         {/* Main Items */}
         <ul className="py-2  bg-gradient-to-b from-[#A279FF] to-[#3B1C83] background-blur-2xl">
           {servicesMenu.map((item, index) => (
@@ -192,18 +193,40 @@ export function ServicesDropdown() {
 
         {/* Subcategories */}
 
-        <div className="flex-1">
+        <div className=" grid grid-cols-2 bg-slate-950 gap-x-100 font-Jost">
           {servicesMenu
             .find((item) => item.label === activeItem)
-            ?.subcategories.map((subcategory, index) => (
-              <li key={index}>
-                <Link
+            ?.subcategories.map((subcategory) => (
+              <div>
+                 <Link
                   href={subcategory.href}
-                  className="block px-4 py-2 text-gray-700 hover:bg-gray-100"
                 >
-                  {subcategory.label}
+
+                  <div className="flex flex-col w-470 p-12">
+
+                    <Image
+                      src={subcategory.imgUrl}
+                      alt={"infogentech"}
+                      width={52}
+                      height={52}/>
+                    <div>
+                       {subcategory.label}
+
+                    </div>
+
+                    <div className="w-85">
+                        {subcategory.description}
+
+                    </div>
+                    
+                  
+                    
+                  </div>
+                 
                 </Link>
-              </li>
+
+              </div>
+               
             ))}
         </div>
       </div>
