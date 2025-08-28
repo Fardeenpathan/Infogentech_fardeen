f7e725e0c3f07f64f39aba164945c7e4f2c38877
Prakash Chand <Prakash0719pc@gmail.com>
2025-08-28 18:38:29 +0530

    refactor: remove unused menu item and improve dropdown

--- src/components/services-dropdown.tsx
+++ src/components/services-dropdown.tsx
@@ -135,12 +135,6 @@ const servicesMenu = [
         imgUrl: "/ServicesSubcategory.svg",
         description: "Creating mobile applications for iOS and Android.",
       },
-      {
-        href: "/services/mobile-app-development",
-        label: "Analysis & Reporting",
-        imgUrl: "/ServicesSubcategory.svg",
-        description: "Creating mobile applications for iOS and Android.",
-      },
     ],
   },
 ];
@@ -155,27 +149,28 @@ export function ServicesDropdown() {
   return (
     <div className="inline-flex relative group">
       <button className="cursor-pointer  focus:outline-none">Services</button>
-      <div className="absolute group-hover:flex  shadow-lg  -left-26 z-50 cursor-pointer ">
+      <div className="absolute group-hover:flex hidden shadow-lg  -left-26 z-50 cursor-pointer ">
         <div className="pt-12 flex">
           <div className="bg-[linear-gradient(213deg,rgba(162,121,255,1)_0%,rgba(63,26,147,1)_89%)]  pl-1 pt-1 rounded-l-md">
             {servicesMenu.map((item, index) => (
               <div
                 key={index}
                 onClick={() => handleItemClick(item.label)}
-                className="flex flex-col py-5.5 w-96 pl-4 hover:bg-[linear-gradient(213deg,rgba(162,121,255,1)_0%,rgba(63,26,147,1)_89%)] rounded-l-md "
+                className="flex flex-col py-5.5 w-96 h-31 pl-4 hover:bg-[linear-gradient(213deg,rgba(162,121,255,1)_0%,rgba(63,26,147,1)_89%)] rounded-l-md "
               >
                 <div className="font-medium text-2xl">{item.label}</div>
                 <div className="justify-center font-['Jost'] leading-6 font-light">{item.description}</div>
               </div>
             ))}
           </div>
-          <div className="grid grid-cols-2 font-Jost bg-[#15152A] w-[950px] border-[#15152A]">
+          <div className="absolute font-['Jost'] right-1/6 font-medium text-[124px] leading-[120%] tracking-[0.03em] top-8 bg-gradient-to-b from-white to-[#15152A] bg-clip-text text-transparent opacity-10">Services</div>
+          <div className="grid grid-cols-2 font-Jost gap-[1px] w-[950px] bg-[#535353] ">
             {servicesMenu
               .find((item) => item.label === activeItem)
               ?.subcategories.map((subcategory, index) => (
                 <div key={index}>
                   <Link href={subcategory.href}>
-                    <div className="flex flex-col pl-12.5 pt-18 pb-10  border-[1px] border-[#535353]">
+                    <div className="flex flex-col pl-12.5 pt-18 pb-10  border-[1px] bg-[#15152A] ">
                       <Image
                         src={subcategory.imgUrl}
                         alt={"infogentech"}
