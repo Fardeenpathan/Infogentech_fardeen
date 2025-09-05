2f4b6a58a2a4a2d3abcbba11cded4b6f4b1d4b91
Prakash Chand <Prakash0719pc@gmail.com>
2025-09-05 17:18:54 +0530

    feat : ui fixed

--- src/components/MoreContact.jsx
+++ src/components/MoreContact.jsx
@@ -9,19 +9,37 @@ export default function MoreContact() {
         <p className="font-[jost] font-medium text-[18px] leading-[24px] align-middle mt-3">
           Check out our FAQs or About Us page for quick answers.
         </p>
-        <div className="flex justify-center items-center gap-20 mt-14 ">
-          <div class="p-[3px] rounded-lg bg-[linear-gradient(106.97deg,#492B8D_10.89%,#5F439D_100.67%)] inline-block">
-            <div class="rounded-lg bg-[#15152a] p-6 w-[313px] h-[183px]">
-              <div className="bg-[#262626] px-5 py-2.5 flex gap-2">
-                <Icons name="QuestionMark" /> <span className="font-jost font-medium text-[20px] leading-[24px] align-middle">FAQs</span>
+        <div className="flex justify-center items-center gap-20 mt-14">
+          <div class="p-[1px] rounded-lg bg-[linear-gradient(106.97deg,#492B8D_10.89%,#5F439D_100.67%)] inline-block">
+            <div class="rounded-lg bg-[#15152a] p-6 w-[313px] h-[173px]">
+              <div className="bg-[#262626] px-5 py-2.5 flex gap-2 w-fit radius">
+                <Icons name="Questionmark" />
+                <span className="font-jost font-medium text-[20px] leading-[24px] align-middle">
+                  FAQs
+                </span>
               </div>
+              <p className="text-[#9CA3AF] font-jost font-medium text-[20px] leading-[24px] align-middle mt-3">
+                Visit our FAQ page.
+              </p>
+              <p className="mt-9">
+                <Icons name="Link" />
+              </p>
             </div>
           </div>
-          <div class="p-[3px] rounded-lg bg-[linear-gradient(106.97deg,#492B8D_10.89%,#5F439D_100.67%)] inline-block">
-            <div class="rounded-lg bg-[#15152a] p-6 w-[313px] h-[183px]">
-              <span className="bg-[#262626] px-5 py-2.5 flex gap-2">
-                <Icons name="People" /> <span className="font-jost font-medium text-[20px] leading-[24px] align-middle">About us</span>
-              </span>
+          <div class="p-[1px] rounded-lg bg-[linear-gradient(106.97deg,#492B8D_10.89%,#5F439D_100.67%)] inline-block">
+            <div class="rounded-lg bg-[#15152a] p-6 w-[313px] h-[173px]">
+              <div className="bg-[#262626] px-5 py-2.5 flex gap-2 w-fit radius">
+                <Icons name="People" />
+                <span className="font-jost font-medium text-[20px] leading-[24px] align-middle">
+                  About Us
+                </span>
+              </div>
+              <p className="text-[#9CA3AF] font-jost font-medium text-[20px] leading-[24px] align-middle mt-3">
+                Visit our Aboutus page.
+              </p>
+              <p className="mt-9">
+                <Icons name="Link" />
+              </p>
             </div>
           </div>
         </div>
