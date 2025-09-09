8b5007ec3f968770ff4dd9c923215f0abb9e6112
Prakash Chand <Prakash0719pc@gmail.com>
2025-09-09 15:29:33 +0530

    create 4 page of resuable for portfolio

---
+++ src/app/portfolio/development/page.jsx
@@ -0,0 +1,70 @@
+"use client";
+import PortfolioHeader from "@/components/PortfolioHeader";
+import PortfolioServices from "@/components/PortfolioServices";
+import SubscribeContact from "@/components/SubscribeContact";
+import Image from "next/image";
+import Icons from "@/components/ui/Icon";
+import Link from "next/link";
+
+const Development = () => {
+  const developmentProjects = [
+    {
+      id: 1,
+      title: "Green House Gardening Landing Page",
+      image: "/assist/img/DesignImg.png",
+    },
+    {
+      id: 2,
+      title: "AI SaaS Dashboard UI",
+      image: "/assist/img/DesignImg.png",
+    },
+  ];
+  return (
+    <>
+      <PortfolioHeader />
+      <PortfolioServices />
+      <div>
+        <div className="container mx-auto mt-10 relative top-48 -z-20">
+          <div className="mx-auto w-fit">
+            <Icons name="CurveGradient" />
+          </div>
+        </div>
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-8 container mx-auto">
+          {developmentProjects?.map((project) => (
+            <div key={project.id} className="p-2.5 rounded-xl">
+              <div className="w-full max-w-[774px]">
+                <Image
+                  src={project.image}
+                  alt={project.title}
+                  width={774}
+                  height={380}
+                  objectFit="cover"
+                  className="rounded-xl"
+                />
+                <div className="px-2.5 pb-2.5 flex justify-between flex-col">
+                  <div className="flex items-center justify-between mt-4">
+                    <p className="font-[jost] text-[24px] font-normal leading-[28px] tracking-normal">
+                      {project.title}
+                    </p>
+                    <Link href="#">
+                      <Icons
+                        name="Arrow"
+                        height={26}
+                        width={59}
+                        color="#C4C4C4"
+                      />
+                    </Link>
+                  </div>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+        <div className="text-[#82828C] mt-24 border-2 container mx-auto"></div>
+        <SubscribeContact />
+      </div>
+    </>
+  );
+};
+
+export default Development;
---
+++ src/components/PortfolioServices.jsx
@@ -0,0 +1,39 @@
+"use client";
+import Icons from "@/components/ui/Icon";
+import Link from "next/link";
+import { useState } from "react";
+
+const PortfolioServices = () => {
+  const [activeService, setActiveService] = useState("Design");
+  const [services, setServices] = useState([
+    { id: 1, icon: "Design", label: "Design", slug: "design", active: true },
+    { id: 2, icon: "Development", label: "Development", slug: "development", active: false },
+    {
+      id: 3,
+      icon: "DigitalMarketing",
+      label: "Digital Marketing",
+      slug: "marketing",
+      active: false,
+    },
+    { id: 4, icon: "Content", label: "Content", slug: "content", active: false },
+  ]);
+
+  return (
+    <div className="flex gap-6 justify-self-center mt-5">
+      {services.map((service) => (
+        <Link
+          key={service.id}
+          href={`/portfolio/${service.slug}`}
+          className={`flex gap-2 items-center justify-center border-[1px] border-white  rounded-md px-7 py-2 cursor-pointer transition-opacity duration-300 ${
+            activeService === service.label ? "" : "opacity-30"
+          }`}
+        >
+          <Icons name={service.icon} />
+          <p className="text-sm font-[jost] ">{service.label}</p>
+        </Link>
+      ))}
+    </div>
+  );
+};
+
+export default PortfolioServices;
