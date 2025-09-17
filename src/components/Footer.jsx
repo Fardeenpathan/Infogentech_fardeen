2c4900c8e16c4ce4b7162c5c725ad72869e45e8b
Prakash Chand <Prakash0719pc@gmail.com>
2025-09-17 18:43:21 +0530

    Feat:create temrs page , responisve footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,29 +5,29 @@ import Button from "./ui/Button";
 
 export function Footer() {
   return (
-    <footer className="bg-[#101021] mx-auto mt-36 relative overflow-hidden px-10">
-      <div className="container mx-auto pt-11 ">
-        <div className="flex justify-between items-center ">
+    <footer className="bg-[#101021] mx-auto mt-36 relative overflow-hidden md:px-10 px-4">
+      <div className="container mx-auto pt-11">
+        <div className="flex md:justify-between items-center md:flex-row flex-col justify-center">
           <div className="w-[162px] h-[162px] -top-[26px] -left-3.5 rounded-[81px] absolute bg-[#8752ff] blur-[100px]" />
           <div className="w-[148px] h-[87px] top-[533px] right-0 rounded-[74px/43.5px] absolute bg-[#8752ff] blur-[100px]" />
-          <p className="font-jost font-medium text-[45px] leading-[60px]">
+          <p className="font-jost font-medium xl:text-[45px] xl:leading-14 leading-6 md:leading-9 text-lg md:text-3xl">
             Start Earlier, Grow Faster <br /> Without Hidden Costs.
           </p>
-          <div className="w-full md:w-1/2 lg:w-1/3 flex justify-end items-start space-x-5 font-jost ">
+          <div className="w-full md:w-1/2 lg:w-1/3 flex justify-center md:justify-end space-x-5 font-jost mt-6">
             <Link
               href="/portfolio"
-              className="bg-white text-black  py-5 px-12.5 rounded-[10px] "
+              className="bg-white text-black  xl:py-5 lg:px-12.5  radius py-3.5 px-8 text-nowrap"
             >
              View Portfolio
             </Link>
             <Link href="/contactUs">
-              <Button text="Contact Us" />
+              <Button text="Contact Us" className="!py-3.5 !px-10 xl:!py-5.5 lg:!px-12.5 text-nowrap"/>
             </Link>
           </div>
         </div>
         <div className=" w-full text-white border-1 mt-7.5"></div>
         <div className="flex flex-wrap justify-between items-center">
-          <div className="w-full grid grid-cols-3 mt-6.5">
+          <div className="w-full grid md:grid-cols-3 grid-cols-1 mt-6.5">
             <div>
               <a className="flex items-center mb-4 gap-1.5" href="/">
                 <Icons name="LogoFooter" />
@@ -70,7 +70,7 @@ export function Footer() {
               </div>
             </div>
             <div></div>
-            <div className="flex flex-wrap justify-between items-start mb-12 font-kumbh-sans">
+            <div className="grid xl:grid-cols-3 grid-cols-2 items-start mb-12 font-kumbh-sans text-nowrap gap-5 mt-10">
               <div className="w-full md:w-1/4 mb-6 md:mb-0">
                 <h3 className="text-lg font-semibold mb-4 font-jost">Links</h3>
                 <ul className="space-y-3.5">
@@ -214,7 +214,7 @@ export function Footer() {
             <Link href="/privacyPolicy" className="hover:text-white ">
               Privacy Policy
             </Link>
-            <Link href="#" className="hover:text-white">
+            <Link href="/terms" className="hover:text-white">
               Terms & Conditions
             </Link>
           </div>
