138d49a3f90bb79b48d5a305def9ffb0a62f3eb0
Prakash Chand <Prakash0719pc@gmail.com>
2025-10-11 11:57:37 +0530

    FEat:all 4 page service add 2 component

---
+++ src/components/india/IndAllServices.jsx
@@ -0,0 +1,64 @@
+"use client";
+import Icons from "@/components/ui/Icon";
+import Link from "next/link";
+
+const IndAllServices = ({ activeService }) => {
+  const services = [
+    { id: 1, icon: "DesignInd", label: "Design", text: "Driving Growth Online", slug: "design" },
+    { id: 2, icon: "DevelopmentInd", label: "Development", text: "Driving Growth Online", slug: "development" },
+    {
+      id: 3,
+      icon: "MarketingInd",
+      label: "Digital Marketing",
+      text: "Driving Growth Online",
+      slug: "marketing",
+    },
+    { id: 4, icon: "ContentInd", label: "Content", text: "Driving Growth Online", slug: "content" },
+  ];
+
+  return (
+ <div className="relative  justify-self-center grid md:grid-cols-4 grid-cols-2 md:mt-5 xl:mt-0 text-nowrap mt-4 rounded-2xl shadow-2xl">
+  {services.map((service, index) => (
+    <div key={service.id} className="relative">
+      {index < services.length - 1 && (
+        <div className="hidden md:block absolute top-10 right-0 h-[60%] w-1 bg-[#B493FF] z-10"></div>
+      )}
+
+      <Link
+        href={`/services/${service.slug}`}
+        scroll={false}
+        className={`flex gap-2 items-center justify-center rounded-md px-16 py-4 cursor-pointer transition-all duration-300 ${
+          activeService === service.label ? " " : ""
+        }`}
+      >
+        <div className={`flex flex-col p-10 justify-center items-center ${activeService === service.label ? "bg-primary scale-140 rounded-2xl mr-1 !px-6" : ""}`}>
+          <Icons
+            name={service.icon}
+            width="42"
+            height="42"
+            className={activeService === service.label ? "text-white" : "text-primary"}
+          />
+          <p
+            className={`font-montserrat text-2xl mb-2.5 mt-4 font-medium ${
+              activeService === service.label ? "text-white" : "text-primary"
+            }`}
+          >
+            {service.label}
+          </p>
+          <p
+            className={`font-montserrat text-lg font-medium ${
+              activeService === service.label ? "text-white" : "text-gray-200"
+            }`}
+          >
+            {service.text}
+          </p>
+        </div>
+      </Link>
+    </div>
+  ))}
+</div>
+
+  );
+};
+
+export default IndAllServices;
---
+++ src/components/india/IndServicesHeader.jsx
@@ -0,0 +1,13 @@
+const IndServicesHeader = ()=>{
+return (
+   <div className="w-full bg-[#E6DBFF] h-158 justify-center items-center flex flex-col">
+        <p className="font-avalors font-normal text-[80px] leading-[1.6] tracking-[3px] align-middle text-primary">
+          Our services
+        </p>
+        <p className="font-avalors font-normal text-[80px] leading-[1.6] tracking-[3px] align-middle text-primary">
+          what we provide
+        </p>
+      </div>
+);
+}
+export default IndServicesHeader;
