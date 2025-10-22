d61aeec7942a7985d90c24aeb10df7c6de652030
Prakash Chand <Prakash0719pc@gmail.com>
2025-10-22 17:39:31 +0530

    Feat:make a resubale component for portfolio page

--- src/app/portfolio/digital-marketing/page.jsx
+++ src/app/portfolio/digital-marketing/page.jsx
@@ -1,74 +1,21 @@
-"use client"
+
 import IndServicesHeader from "@/components/india/IndServicesHeader";
 import IndProtfolioAll from "@/components/india/IndProtfolioAll";
-import Image from "next/image";
-import Icons from "@/components/ui/Icon";
 import IndCustomLine from "@/components/india/IndCustomLine";
-import Link from "next/link";
 import marketingProjects from "./marketingProjects";
-import { motion } from "framer-motion";
+import PortfolioProjectCard from "@/components/PortfolioProjectCard";
 
 const DigitalMarketing = () => {
   return (
     <>
       <IndServicesHeader />
       <IndProtfolioAll activeService="digital-marketing" />
-      <div>
         <div className="grid grid-cols-1 lg:grid-cols-2 container mx-auto">
-          {marketingProjects?.map((project,index) => (
-            <motion.div
-              key={project.id}
-              className="p-2.5 rounded-xl"
-              initial={{ opacity: 0, y: 30 }}
-              whileInView={{ opacity: 1, y: 0 }}
-              transition={{ duration: 0.6, delay: index * 0.1 }}
-              viewport={{ once: true }}
-            >
-              <div className="w-full max-w-[774px]">
-                <div className="w-full max-w-[774px] h-70 md:h-110 lg:h-120">
-                  <Image
-                    src={project.subImage}
-                    alt={project.title}
-                    width={774}
-                    height={380}
-                    className="rounded-xl w-full h-full object-cover"
-                  />
-                </div>
-                <div className="px-2.5 pb-2.5 flex justify-between flex-col">
-                  <div className="flex items-center justify-between mt-4">
-                    <p className="font-montserrat lg:text-2xl md:text-xl text-sm font-normal leading-7 tracking-normal">
-                      {project.title}
-                    </p>
-                    <Link
-                      href={`/portfolio/digital-marketing/${project.slug}`}
-                      className="hidden lg:block"
-                    >
-                      <Icons
-                        name="Arrow"
-                        height={26}
-                        width={59}
-                        color="#141313"
-                      />
-                    </Link>
-                    <Link
-                      href={`/portfolio/digital-marketing/${project.slug}`}
-                      className="block lg:hidden"
-                    >
-                      <Icons
-                        name="Arrow"
-                        height={14}
-                        width={26}
-                        color="#141313"
-                      />
-                    </Link>
-                  </div>
-                </div>
-              </div>
-            </motion.div>
+            {marketingProjects?.map((project, index) => (
+            <PortfolioProjectCard key={project.id} project={project} index={index} />
           ))}
         </div>
         <IndCustomLine />
-      </div>
     </>
   );
 };
