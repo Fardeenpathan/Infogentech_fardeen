30f4518d80b502c87094cb3b71e449c3bcb32da4
Prakash Chand <Prakash0719pc@gmail.com>
2025-10-22 12:11:47 +0530

    minor

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -58,7 +58,7 @@ const AboutUs = () => {
     <div>
       <div className="w-full bg-[#E6DBFF] lg:h-158 h-140 justify-center items-center flex">
         <h1 className="font-avalors font-normal md:text-[80px] text-[46px] leading-[1.6] tracking-[3px] align-middle text-primary">
-          ABOUT US
+          ABOUT US 
         </h1>
       </div>
       <IndAboutImage />
