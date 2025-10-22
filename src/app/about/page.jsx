f5fd061e4a5bb8cff82d9a09bab3071842d1c607
Prakash Chand <Prakash0719pc@gmail.com>
2025-10-22 12:10:34 +0530

    minor

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,9 +1,6 @@
 
-import IndAboutImage from "@/components/india/IndAboutImage";
-import IndAboutDesc from "@/components/india/IndAboutDesc";
-import IndHeroCTA from "@/components/india/IndHeroCTA";
-import IndAchievements from "@/components/india/IndAchievements";
-import IndInfoCards from "@/components/india/IndInfoCards";
+export const dynamic = 'force-static';
+
 export const metadata = {
   title: "About Us | Infogentech Softwares LLP",
   description:
@@ -14,22 +11,25 @@ export const metadata = {
     description:
       "Infogentech Softwares LLP is a leading digital marketing company in India. Our team of experts delivers top-notch services to meet client business requirements.",
     url: "https://infogentech.com/about",
-    siteName: "infogentech",
+    siteName: "Infogentech",
     type: "website",
     images: [
       {
         url: "https://res.cloudinary.com/dpmceu66e/image/upload/v1758791491/blog-app/categories/pvkolpeq7wop6m9lcbs9.jpg",
-        alt: "Infogentech",
+        alt: "Infogentech Office",
+        width: 1200,
+        height: 630,
       },
     ],
   },
   twitter: {
-    card: "summary",
+    card: "summary_large_image",
     title: "About Us | Infogentech Softwares LLP",
     description:
       "Infogentech Softwares LLP is a leading digital marketing company in India. Our team of experts delivers top-notch services to meet client business requirements.",
-    site: "@infogentech",
-    images: ["https://res.cloudinary.com/dpmceu66e/image/upload/v1758791491/blog-app/categories/pvkolpeq7wop6m9lcbs9.jpg"],
+    images: [
+      "https://res.cloudinary.com/dpmceu66e/image/upload/v1758791491/blog-app/categories/pvkolpeq7wop6m9lcbs9.jpg",
+    ],
   },
   robots: "index, follow, all",
   authors: [{ name: "InfoGenTech" }],
@@ -41,25 +41,31 @@ export const metadata = {
     },
   },
   other: {
-    "geo.region": "US",
+    "geo.region": "IN",
     "http-equiv": "x-ua-compatible",
     distribution: "global",
   },
 };
 
+import IndAboutImage from "@/components/india/IndAboutImage";
+import IndAboutDesc from "@/components/india/IndAboutDesc";
+import IndHeroCTA from "@/components/india/IndHeroCTA";
+import IndAchievements from "@/components/india/IndAchievements";
+import IndInfoCards from "@/components/india/IndInfoCards";
+
 const AboutUs = () => {
   return (
     <div>
-      <div className="w-full bg-[#E6DBFF] lg:h-158 h-140  justify-center items-center flex">
+      <div className="w-full bg-[#E6DBFF] lg:h-158 h-140 justify-center items-center flex">
         <h1 className="font-avalors font-normal md:text-[80px] text-[46px] leading-[1.6] tracking-[3px] align-middle text-primary">
           ABOUT US
         </h1>
       </div>
-        <IndAboutImage />
+      <IndAboutImage />
       <IndAboutDesc />
       <IndAchievements />
-      <IndInfoCards/>
-     <IndHeroCTA />
+      <IndInfoCards />
+      <IndHeroCTA />
     </div>
   );
 };
