4fcd2cf119c65c0309d2886cce885732a0cfb58b
Prakash Chand <Prakash0719pc@gmail.com>
2025-10-23 11:37:26 +0530

    Feat : moving banner create

--- src/components/MovingTextBanner.jsx
+++ src/components/MovingTextBanner.jsx
@@ -1,21 +1,23 @@
-"use client"
+
 import Icons from "@/components/ui/Icon";
 
 const MovingTextBanner = () => {
   return (
-    <div className="moving-text-container overflow-hidden bg-[#3F237F] text-white my-30">
-        <div className="moving-text-content font-montserrat font-normal text-xl -tracking-[0.05em] custom-skew">
+    <div className="relative my-30">
+      <div className="absolute inset-0 bg-primary -z-10"></div>
+      <div className="moving-text-container overflow-hidden bg-[#3F237F] text-white relative z-10 custom-skew">
+        <div className="moving-text-content font-montserrat font-normal text-xl -tracking-[0.05em]">
           <div className="flex gap-96">
             {Array.from({ length: 20 }, (_, i) => (
               <p key={i} className="md:py-4 py-2 flex gap-28">
-                Design <Icons name="IndStar" /> Development
-                <Icons name="IndStar" /> Branding <Icons name="IndStar" />
-                Product
+                Design <Icons name="IndStar" /> Development <Icons name="IndStar" /> 
+                Branding <Icons name="IndStar" /> Product
               </p>
             ))}
           </div>
         </div>
       </div>
+    </div>
   );
 };
 
--- src/components/india/IndFaqsHome.jsx
+++ src/components/india/IndFaqsHome.jsx
@@ -14,20 +14,62 @@ const Faqs = [
   },
   {
     id: "02",
-    question: "What is the uptime guarantee for your hosting services?",
-    answer: "We guarantee an uptime of 99.9% for all our hosting services.",
+    question: "What are the differences between SEO and PCC?",
+    answer: "SEO means Search Engine Optimization, and it is aimed at raising the organic position a person has, thus raising the number of visits received over the course of time that are regarded as free. In the meantime, PPC utilizes paid-in adverts that display in search results or social media postings, which attract built-in traffic and pay for the aftermath of each and every click that transfers users to its business.",
   },
   {
     id: "03",
-    question: "What types of hosting plans do you offer?",
+    question: "Do I have to be active on all social platforms?",
     answer:
-      "We offer shared hosting, VPS hosting, dedicated server hosting, and cloud hosting plans.",
+      "No. It will be more effective to work on those platforms that your target audience uses more. The customization approach means that the content is consistent, more people will be engaged, and your marketing costs will be put to better use.",
   },
   {
     id: "04",
-    question: "What is the uptime guarantee for your hosting services?",
-    answer: "We guarantee an uptime of 99.9% for all our hosting services.",
+    question: "Which digital marketing strategy is the right one",
+    answer: "A deeper audit or consultation process will determine what you want to achieve, the type of target you want to target, trends in the industry, and competitors. Informed recommendations are provided based on the goals, wherein the recommendations could be in terms of SEO, content marketing, PPC, or social media campaigns.",
   },
+   {
+    id: "05",
+    question: "Is it possible to use digital market marketing with small or local businesses?",
+    answer:
+      "Yes! Local SEO, location-based advertisements, and social media marketing allow small businesses to find clients in their area to develop brand loyalty, and in the local marketplace, and to compete, even with just a modest budget.",
+  },
+  {
+    id: "06",
+    question: "How do I track results of digital marketing?",
+    answer:
+      "Classifying results can be done by tools such as Google Analytics, social media insights, and reports of campaign performance. Metrics can span traffic clicks to the website, conversions, engagements, click rate, and ROI in ensuring feedback to help improve tactics over time.",
+  },
+  // {
+  //   id: "07",
+  //   question: "What digital marketing services do you offers?",
+  //   answer:
+  //     "We provide SEO, PPC ad agency services, social media management, content marketing, email marketing, and analytics. We seek to assist in converting, increasing visibility, and generating measurable growth for your brand across the digital channels.",
+  // },
+  // {
+  //   id: "08",
+  //   question: "What tools do agencies use for digital marketing?",
+  //   answer:
+  //     "To track and optimize the campaigns, services like Google Analytics, keyword lookups (e.g., SEMrush), social media calendar, email marketing software, and designing tools like Canva or Adobe would need to be planned to be used.",
+  // },
+  // {
+  //   id: "09",
+  //   question: "Do you provide social media marketing services?",
+  //   answer:
+  //     "We can do full social marketing, including creating the content, managing your account, paid campaigns, adverts, and engaging people on social media. We develop your brand over such platforms as Instagram, Facebook, LinkedIn, and Twitter..",
+  // },
+  // {
+  //   id: "10",
+  //   question: "How can SEO assist in the growth of my company?",
+  //   answer:
+  //     "SEO increases your visibility online through search engines. So, when potential customers go online to find people who they are likely to purchase in your place, they will find you. Free publicity, in turn, through more visibility, relevant traffic, and good content, will create organic growth in the long term and trust towards the audience.",
+  // },
+  // {
+  //   id: "11",
+  //   question: "Can you handle PPC Campaigns for my Business?",
+  //   answer:
+  //     "Sure thing. Our team manages Google Ads, Facebook Ads, amongst other PPC campaigns, directing targeted traffic and leads. Our experts monitor the performance of such campaigns, optimize the budgets, and refine the targeting to optimize conversions and minimize wastage of ad expenditure.",
+  // },
 ];
 
 const IndFaqsHome = () => {
