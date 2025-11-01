fc864bcce27ea419acd736620600a039d242b996
Prakash Chand <Prakash0719pc@gmail.com>
2025-11-01 14:49:02 +0530

    Feat: add robot.txt file dynamic generate

---
+++ src/app/robots.js
@@ -0,0 +1,28 @@
+export default function robots() {
+  return {
+    rules: [
+      {
+        userAgent: 'GPTBot',
+        allow: '/',
+      },
+      {
+        userAgent: 'Google-Extended',
+        allow: '/',
+      },
+      {
+        userAgent: '*',
+        disallow: [
+          '/admin/',
+          '/login/',
+          '/cart/',
+          '/checkout/',
+          '/user/',
+          '/private/',
+        ],
+        allow: '/',
+      },
+    ],
+    sitemap: 'https://infogentech.com/sitemap.xml',
+    host: 'https://infogentech.com',
+  };
+}
