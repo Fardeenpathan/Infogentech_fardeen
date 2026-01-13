19a90ded69a13650aa4dfe748273cd0fbfcb057a
fardeenpathan <md.fardeen5236@gmail.com>
2026-01-13 15:30:46 +0530

    geo api key added

--- middleware.js
+++ middleware.js
@@ -1,6 +1,4 @@
-﻿
-
-import { NextResponse } from 'next/server';
+﻿import { NextResponse } from 'next/server';
 
 export async function middleware(request) {
   const { pathname } = request.nextUrl;
