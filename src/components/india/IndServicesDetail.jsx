5a2c91a65eb04f7679ec82c7aac13a64d61a3268
fardeenpathan <md.fardeen5236@gmail.com>
2026-01-17 13:23:00 +0530

    add service details blog

--- src/components/india/IndServicesDetail.jsx
+++ src/components/india/IndServicesDetail.jsx
@@ -6,13 +6,13 @@ export default function IndServiceDetails() {
             <div>
                 <img src="https://res.cloudinary.com/dpmceu66e/image/upload/v1762256798/blog-app/blogs/blogs/1762256795276-frame_338.png" alt="" className="rounded-xl w-full h-108" />
                 <div className="pt-12 pb-8">
-                    <h2 className="text-[24px] font-montserrat font-medium pb-2 text-black">1. Design functional website fast</h2>
+                    <h2 className="text-[24px] font-montserrat font-semibold pb-2 text-black">1. Design functional website fast</h2>
                     <p className="font-montserrat text-[18px] text-gray-400 pr-2">
                         When you’re designing your site, it’s easy to get caught up in adding too many elements to your pages. You want your site to be visually appealing, you continually add elements to add another dimension of visual interest to your page. This can lead to your site getting cluttered and overwhelming. The other issue is that, if certain elements don’t have a purpose on your site, it can leave your audience confused. Every element on your site should have a purpose. Imagine putting a call to action (CTA) button that doesn’t lead to a new page.
                     </p>
                 </div>
                 <div className="pb-8">
-                    <h2 className="text-[24px] font-montserrat font-medium pb-2 text-black">2. Design for the user first</h2>
+                    <h2 className="text-[24px] font-montserrat font-semibold pb-2 text-black">2. Design for the user first</h2>
                     <p className="font-montserrat text-[18px] text-gray-400 pr-2">
                         People would constantly try to click on the button, and it would leave them confused as to why it doesn’t take them somewhere. Or, think what would happen if a website design prompted users to “Call now” on a top-of-the-funnel blog post. People probably wouldn’t click. Create elements that are functional and enhance the user experience on your site. </p>
                 </div>
@@ -27,14 +27,10 @@ export default function IndServiceDetails() {
 
            
         </div>
-         <div className="py-12">
+         <div className="pt-12">
                 <hr className="w-full border-t-4"/>
             </div>
 
-            <div>
-                <h2 className="font-avalors text-3xl text-primary">FAQ</h2>
-                <h2 className="font-avalors text-4xl text-black pt-2">Everything You Need to Know</h2>
-            </div>
             </>
     );
 }
