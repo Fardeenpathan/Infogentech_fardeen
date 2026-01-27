fc505ce5aef809dc0b2bb81b13955666498a4ce9
fardeenpathan <md.fardeen5236@gmail.com>
2026-01-27 17:03:50 +0530

    add padding

--- src/components/india/IndServicesDetail.jsx
+++ src/components/india/IndServicesDetail.jsx
@@ -2,7 +2,7 @@ export default function IndServiceDetails({ servicedetailcontent }) {
     return (
         <>
                 <div>
-                    <img src={servicedetailcontent.img1} alt="" className="rounded-xl w-full h-108" />
+                    <img src={servicedetailcontent.img1} alt="" className="rounded-xl w-full lg:h-108 h-60 object-cover" />
                     <div className="pt-12 pb-8">
                         <h2 className="text-[24px] font-montserrat font-semibold pb-2 text-black">{servicedetailcontent.heading1}</h2>
                         <p className="font-montserrat text-[18px] text-gray-400 pr-2">
@@ -14,9 +14,9 @@ export default function IndServiceDetails({ servicedetailcontent }) {
                             {servicedetailcontent.para2} </p>
                     </div>
                     <div className="grid lg:grid-cols-3 grid-cols-1 gap-6 pb-8">
-                        <img src={servicedetailcontent.img2} alt="" className="" />
-                        <img src={servicedetailcontent.img3} alt="" className="" />
-                        <img src={servicedetailcontent.img4} alt="" className="" />
+                        <img src={servicedetailcontent.img2} alt="" className="object-cover w-full h-48 rounded-2xl" />
+                        <img src={servicedetailcontent.img3} alt="" className="object-cover w-full h-48 rounded-2xl" />
+                        <img src={servicedetailcontent.img4} alt="" className="object-cover w-full h-48 rounded-2xl" />
                     </div>
                     <p className="font-montserrat text-[18px] text-gray-400 pr-2">{servicedetailcontent.para3}</p>
                 </div>
