6393e3236765543b6e15583669d37998759ea5f9
unknown <md.fardeen5236@gmail.com>
2026-02-16 16:36:48 +0530

    do some changes

--- src/components/india/IndServicesDetailsidebar.jsx
+++ src/components/india/IndServicesDetailsidebar.jsx
@@ -78,7 +78,6 @@ export default function IndServicesDetailsidebar() {
                             </SwiperSlide>
                         ))}
                     </Swiper>
-
                     {/* end of testiminal */}
 
                 </div>
